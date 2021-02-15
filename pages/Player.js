f247e23106fe7f80767f303c312182320eff3f99
billymoroney <YOUR-EMAIL-ADDRESS>
2021-02-14 21:07:00 -0600

    partial setup of player page, working towards populating a dropdown menu with names of users presets, storing all presets in state. then set up sequencing

--- pages/Player.js
+++ pages/Player.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+//services
+import { getPresets } from '../services/preset.service'
+
+//components
+import Dropdown from '../components/Synth/Dropdown'
 
 import styles from '../styles/Home.module.css'
 
@@ -7,11 +13,39 @@ import Layout from '../components/Layout'
 
 
 export default function Player() {
+
+    const [presets, setPresets] = useState([])
+    //preset names
+    const [options, setOptions] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    //need to get all presets, populate a dropdown menu with names
+
+    useEffect(() => {
+        getPresets().then((response) => {
+            setPresets(response.data)
+            setLoading(false)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }, [])
+
+    const popNames = () => {
+        const effects = presets.effects
+    }
+
+    //make a new component, sequence, takes preset data. need to extract some logic from trigger component to reuse 
+    //sequences component should also extract waveform data to manipulate canvas component?
+
+
     return (
         <Layout>
             <div className={styles.container}>
                 Player
             </div>
+            {!loading && (
+                <Dropdown name='waveType' options={['sine', 'triangle', 'square', 'sawtooth']} />
+            )}
         </Layout>
     )
 }
